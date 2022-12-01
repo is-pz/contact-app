f29e3b8aa830dc25d69ba98684d98585d540d860
Isra <israel_paz@msn.com>
2022-12-01 12:13:14 -0600

    validations and bindParams

--- add.php
+++ add.php
@@ -2,18 +2,29 @@
 
 require_once 'database.php';
 
+$error = null;
+
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-  $name = $_POST['name'];
-  $phoneNumber = $_POST['phone_number'];
- 
-  $stmt = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES ('$name', '$phoneNumber')");
+  
+  if(empty($_POST['name']) || empty($_POST['phone_number'])){
+    $error = 'Please fill all fields.';
+  }else if(strlen($_POST["phone_number"]) < 9){
+    $error = "Phone number must be at least 9 characters.";
+  }else{
 
-  $stmt->execute();
+    $name = $_POST['name'];
+    $phoneNumber = $_POST['phone_number'];
+  
+    $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES (:name, :phone_number)");
+    $statement->bindParam(":name", $_POST["name"]);
+    $statement->bindParam(":phone_number", $_POST["phone_number"]);
+    $statement->execute();
+  
+    $stmt = null;
+    header("Location: index.php");
+  }
 
-  $stmt = null;
-
-  header("Location: index.php");
 }
 
 ?>
@@ -84,6 +95,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
           <div class="card">
             <div class="card-header">Add New Contact</div>
             <div class="card-body">
+              <?php if ($error): ?>
+                <p class="text-danger text-center">
+                  <?= $error ?>
+                </p>
+              <?php endif ?>
               <form method="POST" action="add.php">
                 <div class="mb-3 row">
                   <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
